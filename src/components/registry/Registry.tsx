7d8b800a2e38a50cde2f94d173407fe396353a19
Jesus <jesus.librado@vwfs.io>
2021-06-29 20:00:12 -0500

    Registry template

--- src/components/registry/Registry.tsx
+++ src/components/registry/Registry.tsx
@@ -54,5 +54,107 @@ export const Registry: React.FC = () => (
             ></input>
             <div className="valid-feedback">Correcto</div>
         </div>
+        <div className="col-md-4 col-sm-6">
+            <label htmlFor="sexo" className="form-label">
+                Sexo
+            </label>
+            <select className="form-select is-invalid" id="sexo" required>
+                <option selected>Ver opciones</option>
+                <option value="MASCULINO">Masculino</option>
+                <option value="FEMENINO">Femenino</option>
+                <option value="OTRO">Otro</option>
+            </select>
+            <div className="invalid-feedback">Incorrecto</div>
+        </div>
+        <div className="col-md-12"></div>
+        <div className="col-md-3 col-sm-6">
+            <label htmlFor="telefono" className="form-label">
+                Telefono
+            </label>
+            <input
+                type="text"
+                className="form-control is-invalid"
+                id="telefono"
+                value="0123456789"
+                required
+            ></input>
+            <div className="invalid-feedback">Incorrecto</div>
+        </div>
+        <div className="col-md-5 col-sm-6">
+            <label htmlFor="email" className="form-label">
+                Correo Electronico
+            </label>
+            <input
+                type="email"
+                className="form-control is-invalid"
+                id="email"
+                value="example@domain.com"
+                required
+            ></input>
+            <div className="invalid-feedback">Incorrecto</div>
+        </div>
+        <div className="col-md-12"></div>
+        <div className="col-md-7 col-sm-12">
+            <label htmlFor="address" className="form-label">
+                Direccion
+            </label>
+            <input
+                type="text"
+                className="form-control is-invalid"
+                id="address"
+                value="example@domain.com"
+                required
+            ></input>
+            <div className="invalid-feedback">Incorrecto</div>
+        </div>
+        <div className="col-md-5 col-sm-6">
+            <label htmlFor="colonia" className="form-label">
+                Colonia
+            </label>
+            <input
+                type="text"
+                className="form-control is-invalid"
+                id="address"
+                value="Centro"
+                required
+            ></input>
+            <div className="invalid-feedback">Incorrecto</div>
+        </div>
+        <div className="col-md-4 col-sm-6">
+            <label htmlFor="city" className="form-label">
+                Ciudad
+            </label>
+            <input
+                type="text"
+                className="form-control is-invalid"
+                id="city"
+                value="Cordoba"
+                required
+            ></input>
+            <div className="invalid-feedback">Incorrecto</div>
+        </div>
+        <div className="col-md-4 col-sm-6">
+            <label htmlFor="state" className="form-label">
+                Estado
+            </label>
+            <select className="form-select is-invalid" id="state" required>
+                <option selected>Ver opciones</option>
+                <option value="VERACRUZ">Veracruz</option>
+            </select>
+            <div className="invalid-feedback">Incorrecto</div>
+        </div>
+        <div className="col-md-4 col-sm-6">
+            <label htmlFor="codigoPostal" className="form-label">
+                Codigo Postal
+            </label>
+            <input
+                type="text"
+                className="form-control is-invalid"
+                id="codigoPostal"
+                value="72653"
+                required
+            ></input>
+            <div className="invalid-feedback">Incorrecto</div>
+        </div>
     </div>
 );
